788158be0550d43359a15be618165ef871daccde
safirarmdhnn <153653194+safirarmdhnn@users.noreply.github.com>
2023-12-13 01:04:33 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// Toggle class active
+const navbarNav = document.querySelector(".navbar-nav");
+document.querySelector("hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
